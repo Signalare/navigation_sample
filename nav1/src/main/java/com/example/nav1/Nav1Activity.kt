607c303112a5179d52db45b2c702939f5cacd212
Marcus Hermansson <marcus@signalare.se>
2018-08-02 20:53:41 +0200

    First commit

---
+++ nav1/src/main/java/com/example/nav1/Nav1Activity.kt
@@ -0,0 +1,12 @@
+package com.example.nav1
+
+import android.app.Activity
+import android.os.Bundle
+
+class Nav1Activity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nav1)
+    }
+}
---
+++ nav2/src/main/java/com/example/nav2/Nav2Activity.kt
@@ -0,0 +1,13 @@
+package com.example.nav2
+
+import android.app.Activity
+import android.os.Bundle
+
+class Nav2Activity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nav2)
+
+    }
+}
